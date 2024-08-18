9077190193f49faa522d782170b6187e6b1fe69c
AhmadQasem37 <150855336+AhmadQasem37@users.noreply.github.com>
2024-08-18 07:39:47 +0300

    ADD task three operations

--- Program.cs
+++ Program.cs
@@ -11,7 +11,7 @@ namespace BankApp
             List<AccountHolder> holders = new List<AccountHolder>();
             string[] names = { "ahmad", "ali", "mohammed", "yamen", "reem", "lana" };
 
-            int length = random.Next(1,5);
+            int length = random.Next(1, 5);
             for (int i = 0; i < length; i++)
             {
                 accounts.Add(new BankAccount(random.Next(100, 501), random.Next(10000), random.Next(10000000, 100000000).ToString()));
@@ -21,9 +21,28 @@ namespace BankApp
 
             for (int i = 0; i < length; i++)
             {
-                accounts[i].Deposit(holders[i], random.Next(100, 501));
-                accounts[i].Withdraw(holders[i], random.Next(100, 501));
+
+
+                for (int j = 0; j < 2; j++)
+                {
+                    int operation = random.Next(1, 3);
+                    if (operation == 1)
+                    {
+                        accounts[i].Deposit(holders[i], random.Next(100, 501));
+                    }
+                    else
+                    {
+                        accounts[i].Withdraw(holders[i], random.Next(100, 501));
+                    }
+                }
+                Console.WriteLine("Transaction History:");
+                foreach (var transaction in accounts[i].TransactionHistory)
+                {
+                    Console.WriteLine(transaction);
+                }
+
                 Console.WriteLine("\n--------------------------------------------------------------\n");
+
 
             }
         }
